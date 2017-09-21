b4d20db1db6a445eaaa3a8a951a50aab49699058
Ilya Sytchev <hackdna@gmail.com>
2017-09-20 23:08:22 -0400

    Add S3 bucket for remote state storage and switch to us-east-2 region

---
+++ s3/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-example"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-example"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
