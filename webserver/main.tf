a794945c3ee14c9354e470eb652b87bd10cff383
Ilya Sytchev <hackdna@gmail.com>
2017-09-21 17:42:14 -0400

    Switch webserver cluster AMI to us-east-2 region

--- webserver/main.tf
+++ webserver/main.tf
@@ -8,7 +8,7 @@ variable "server_port" {
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-40d28157"
+  image_id        = "ami-153e6470"
   instance_type   = "t2.micro"
   security_groups = [
     "${aws_security_group.instance.id}"
