6a652c1a4ff82dd02cd9f5a4a50f13a8c040c2ac
Ilya Sytchev <hackdna@gmail.com>
2017-09-26 12:06:38 -0400

    Enable conditional user_data scripts

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -9,6 +9,7 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
+  count = "${1 - var.enable_new_user_data}"
   template = "${file("${path.module}/user-data.sh")}"
 
   vars {
@@ -18,13 +19,26 @@ data "template_file" "user_data" {
   }
 }
 
+data "template_file" "user_data_new" {
+  count = "${var.enable_new_user_data}"
+
+  template = "${file("${path.module}/user-data-new.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-153e6470"
   instance_type   = "${var.instance_type}"
   security_groups = [
     "${aws_security_group.instance.id}"
   ]
-  user_data       = "${data.template_file.user_data.rendered}"
+  user_data       = "${element(
+    concat(data.template_file.user_data.*.rendered,
+           data.template_file.user_data_new.*.rendered),
+    0)}"
 
   lifecycle {
     create_before_destroy = true
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -13,6 +13,7 @@ module "webserver_cluster" {
   min_size = 2
   max_size = 2
   enable_autoscaling = false
+  enable_new_user_data = true
 }
 
 terraform {
